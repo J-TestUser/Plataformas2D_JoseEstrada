8d31e8c6613153d8273029f7509d58f3e39ced5c
J-TestUser <jestrada@ctsformacion.com>
2026-09-16 13:56:01 +0200

    CharacterController2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,23 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
     [SerializeField] private int _maxHealth = 100;
     [SerializeField] private float movementSpeed = 4.5f;    
-    [SerializeField] private float _jumpForce = 7;
+    [SerializeField] private float _jumpHeight = 10;
+
+//Components
     private Rigidbody2D _rigidBody2D;
+
+//Inputs
     private InputAction _moveAction;
     private InputAction _jumpAction;
     private Vector2 _moveInput;
 
+//Ground Sensor
+    [SerializeField] private Transform _groundSensor;
+    [SerializeField] private float _sensorSize = 1;
+    [SerializeField] private LayerMask _groundLayer;
 
     void Awake()
     {
@@ -32,7 +41,7 @@ public class PlayerController : MonoBehaviour
         }
 
 
-        if (_jumpAction.WasPressedThisFrame())
+        if (_jumpAction.WasPressedThisFrame() && IsGrounded())
         {
             Jump();
         } 
@@ -46,9 +55,27 @@ public class PlayerController : MonoBehaviour
 
     void Jump()
     {
-        _rigidBody2D.AddForce(Vector2.up * _jumpForce,ForceMode2D.Impulse);
+        _rigidBody2D.AddForce(Vector2.up * Mathf.Sqrt(_jumpHeight * -2 * Physics2D.gravity.y),ForceMode2D.Impulse); 
     }
 
+    bool IsGrounded()
+    {
+        Collider2D[] colliders2D = Physics2D.OverlapCircleAll(_groundSensor.position, _sensorSize);
+
+        foreach (Collider2D item in colliders2D)
+        {
+            if(item.gameObject.layer == 6)
+            {
+                return true;
+            }
+        }
+        return false; 
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(_groundSensor.position, _sensorSize);
+    }
 
 }
 
